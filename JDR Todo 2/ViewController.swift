c8b6dc5eea6ca8b35d6d6f50f5df650f6f1e8e67
Jayden H Jang <Newcephei@icloud.com>
2023-06-10 08:18:13 +0900

    Initial Commit

---
+++ JDR Todo 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JDR Todo 2
+//
+//  Created by Jayden Jang on 2023/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JDR Todo 2UITests/JDR_Todo_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JDR_Todo_2UITestsLaunchTests.swift
+//  JDR Todo 2UITests
+//
+//  Created by Jayden Jang on 2023/06/10.
+//
+
+import XCTest
+
+final class JDR_Todo_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
